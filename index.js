aee6185fb8bc6c75165e9eaf8d38e18cb050d016
Breiner E. Hernández G <ing.breiner.hernandez@gmail.com>
2021-05-31 17:31:11 -0500

    crud completo en memoria

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const express = require("express");
 const app = express();
 
+// parse application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+// parse application/json
+app.use(express.json());
+
 app.use(require("./mod/router"));
 
-app.listen(4500, () => {
+// app.listen(4500, () => {
+//   console.log("Server running on port 4500");
+// });
+
+app.listen(4500, function () {
   console.log("Server running on port 4500");
 });
--- mod/nomina/controllers/empleados.js
+++ mod/nomina/controllers/empleados.js
@@ -1,14 +1,34 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
+var lista = ["Diana", "Sofia"];
+
+const saludar = (nombre) => `Hola, ${nombre}`;
+
+// app.get("/", (req, res) => {
+//   res.json({
+//     ok: true,
+//     msg: `Lista de empleados.`,
+//     data: lista,
+//   });
+// });
+
+app.get("/", function (req, res) {
+  const [diana, sofia] = lista;
+  console.log(diana, sofia);
+
   res.json({
     ok: true,
     msg: `Lista de empleados.`,
+    data: lista,
   });
 });
 
 app.post("/", (req, res) => {
+  const { nombre } = req.body;
+
+  lista.push(nombre);
+
   res.json({
     ok: true,
     msg: `Se ha creado un registro satisfactoriamente`,
@@ -17,6 +37,10 @@ app.post("/", (req, res) => {
 
 app.put("/:id", (req, res) => {
   const { id } = req.params;
+  const { nombre } = req.body;
+
+  lista[id] = nombre;
+
   res.json({
     ok: true,
     msg: `Se ha modificado un registro satisfactoriamente el registro ${id}`,
@@ -25,9 +49,33 @@ app.put("/:id", (req, res) => {
 
 app.delete("/:id", (req, res) => {
   const { id } = req.params;
+
+  var listaTmp = [];
+  lista.map((elemento, key) => {
+    if (key != id) {
+      listaTmp.push(elemento);
+    }
+  });
+
+  lista = listaTmp;
+
   res.json({
     ok: true,
     msg: `Se ha eliminado un registro satisfactoriamente el registro ${id}`,
+  });
+});
+
+app.get("/:id", (req, res) => {
+  const { id } = req.params;
+
+  lista.map((elemento, key) => {
+    if (key == id) {
+      res.json({
+        ok: true,
+        msg: `Se entrega información del registro ${id}`,
+        data: saludar(elemento),
+      });
+    }
   });
 });
 
